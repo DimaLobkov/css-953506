81e16d3f4eed5e5fd82c6fdfe88f318f18746272
BoyNextDoorxBY <egkuzoff@gmail.com>
2020-04-02 02:07:27 +0300

    Initialization commit for Lab 5.
    Added a stub for two classes and readme.

---
+++ kuzovkov.yegor/Lab 5/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Lab_5_Ind_1
+{
+	 //TODO:
+    //1.Most important: add indexers(if possible).
+    //2.Add enum CarType.
+    class Car : Transport
+    {
+        //fields
+        protected uint price = 0;
+        protected uint numberOfSeats;
+        protected uint power;
+        protected uint trunkSize;
+
+        //properties
+        public uint Price
+        {
+            get => price;
+            set => price = value;
+        }
+
+        public uint Seats
+        {
+            get => numberOfSeats;
+            set => numberOfSeats = value;
+        }
+
+        public uint Power
+        {
+            get => power;
+            set => power = value;
+        }
+
+        public uint TrunkSize
+        {
+            get => trunkSize;
+            set => trunkSize = value;
+        }
+
+        //constructors
+        public Car(uint carPrice, uint carSeats, uint carPower, uint carSize)
+        { 
+            price = carPrice;
+            numberOfSeats = carSeats;
+            power = carPower;
+            trunkSize = carSize;
+        }
+
+        public Car()
+        {
+            price = 0;
+            numberOfSeats = 0;
+            power = 0;
+            trunkSize = 0;
+        }
+
+        //destructor
+        ~Car(){}
+
+        //methods
+        public void Calculate()
+        {
+            price = numberOfSeats * 2000 + power * 2;
+            if (ComfortLevel == "high")
+                price *= 3;
+            else if (ComfortLevel == "medium")
+                price *= 2;
+        }
+    }
+}
---
+++ kuzovkov.yegor/Lab 5/Transport.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Lab_5_Ind_1
+{
+    abstract class Transport
+    {
+        //fields
+        protected static int _seriesNumber;
+        protected string _name;
+        protected string _color;
+        protected string _comfortLevel;
+        protected uint _yearMade;
+
+        //properties
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Color
+        {
+            get => _color;
+            set => _color = value;
+        }
+
+        public string ComfortLevel
+        {
+            get => _comfortLevel;
+            set => _comfortLevel = value;
+        }
+
+        public uint YearMade
+        {
+            get => _yearMade;
+            set
+            {
+                if (value > 2020 || value < 1960)
+                    return;
+                else
+                    _yearMade = value;
+            }
+        }
+
+        public static int Series
+        {
+            get => _seriesNumber;
+        }
+
+        //constructors
+        public Transport(string name, string color, string comfort, uint year)
+        {
+            _name = Name;
+            _color = Color;
+            _comfortLevel = comfort;
+            _yearMade = year;
+
+        }
+
+        public Transport()
+        {
+            _name = "NULL";
+            _color = "NULL";
+            _comfortLevel = "NULL";
+            _yearMade = 0;
+        }
+
+        //destructor
+        ~Transport() { }
+
+        //methods
+        public static void Beep()
+        {
+            Console.WriteLine("Beep!\a");
+        }
+
+        public static void SetSeries()
+        {
+            Random rnd = new Random();
+            _seriesNumber = rnd.Next(1000, 10000);
+        }
+    }
+}
