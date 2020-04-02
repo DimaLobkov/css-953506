fc14ec8e13a68e2c727a935cdcedeeae450bcda7
vladzimir06 <62436317+vladzimir06@users.noreply.github.com>
2020-04-02 14:02:30 +0300

    2

---
+++ Koshuba.vladzimir/2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace _2_stepen_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong a, b, n = 0;
+            while (true)
+            {
+                Console.WriteLine("must a < b");
+                Console.Write("a = ");
+                a = Convert.ToUInt64(Console.ReadLine());
+                Console.Write("b = ");
+                b = Convert.ToUInt64(Console.ReadLine());
+                if (a >= b)
+                {
+                    Console.Clear();
+                    Console.WriteLine("ERROR");
+                }
+                else break;
+            }
+
+            BigInteger c = 1;
+            for (c = a; c <= b; c++)
+            {
+                BigInteger d = c;
+                while (d % 2 == 0)
+                {
+                    d /= 2;
+                    n++;
+                }
+            }
+            Console.WriteLine($"\nstepen' of 2 {n}");
+            Console.WriteLine("\n\ncontinue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
