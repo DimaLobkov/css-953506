b35ce0e5e4b67a08a7f87dc69350647a8f068fc2
Dubrovskiy-Konstantin <62993410+Dubrovskiy-Konstantin@users.noreply.github.com>
2020-04-03 22:19:06 +0300

    Add files via upload

---
+++ Dubrovskiy-Konstantin/laba3/Class1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Person
+    {        
+        protected string name;    // Имя
+        protected int age;        // Возраст       
+
+        // Реализация свойств класса:
+        public string Name { set; get; }  // Автосвойство (сокр. форма записи свойств)
+        //{
+        //    set
+        //    {
+        //        name = value;
+        //    }
+        //    get
+        //    {
+        //        return name;
+        //    }
+        //}
+
+        public int Age  
+        {
+            set
+            {
+                if (value < 18) Console.WriteLine("Возраст {0} не подходит. Возраст должен быть больше 18 лет", value);
+                else age = value;
+            }
+            get
+            {
+                return age;
+            }
+        }       
+    }
+
+    public class Worker : Person
+    {
+        protected string speciality;  // Специальность
+        protected int experience;     // Опыт работы (лет)
+        protected int salary;         // Базовая з/п (без учета опыта)
+
+        //Реализация статических св-в класса (в данном примере подсчет количества созданных экземпляров Worker)
+        protected static int counter = 0;
+        public Worker()
+        {
+            counter++;
+        }
+        public static int Counter
+        {
+            get
+            {
+                return counter;
+            }
+        }
+
+        //Реализация индексатора (в данном примере по названию поля возвращается его значение (строкой))
+        public string this[string propname]
+        {
+            get
+            {
+                switch (propname)
+                {
+                    case "speciality": return speciality;
+                    case "experience": return experience.ToString();
+                    case "salary": return salary.ToString();
+                    default: return null;
+                }
+            }
+        }
+
+
+        public void Filling(string name, int age, string speciality, int experience)    // полное заполнение 
+        {
+            this.Name = name;
+            this.age = age;
+            this.speciality = speciality;
+            this.experience = experience;
+
+            if (speciality == "dishwasher") salary = 80;        //
+            else if (speciality == "waiter") salary = 100;      //  Базовая з/п различных специальностей
+            else if (speciality == "cook") salary = 150;        //
+            else if (speciality == "manager") salary = 200;     //
+            else salary = 0;    // В иных случаях
+        }
+
+        public void Filling(string speciality, int experience)  // перегруженный метод для сокр. заполнения
+        {            
+            this.speciality = speciality;
+            this.experience = experience;
+
+            if (speciality == "dishwasher") salary = 80;        //
+            else if (speciality == "waiter") salary = 100;      //  Базовая з/п различных специальностей
+            else if (speciality == "cook") salary = 150;        //
+            else if (speciality == "manager") salary = 200;     //
+            else salary = 0;    // В иных случаях
+        }
+
+        public double Payroll()     //расчет з/п
+        {   
+            double coefficient = 1 + (0.2 * experience);  // Расчет коэффициента з/п в зависимости от опыта
+            return coefficient * salary;    // Возвращаем значение з/п
+        }
+
+        public void PrintInfo()
+        {
+            double pay = Payroll();
+
+            if (Name != null && age != 0) Console.WriteLine("Имя: {0}  \t Возраст: {1} \t", Name, age.ToString());            
+            Console.WriteLine("Специальность: {0} \t Базовая з/п: {1} \t Опыт: {2}", speciality, salary.ToString(), experience.ToString());            
+            Console.WriteLine("З/п с учетом опыта: {0}", pay.ToString());
+        }
+    }
+}
---
+++ Dubrovskiy-Konstantin/laba3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            //----------- КЛАСС ЧЕЛОВЕК -> РАБОТНИК -> РАБОТНИКИ РАЗНЫХ СПЕЦИАЛЬНОСТЕЙ ----------------------
+
+            Worker worker1 = new Worker();           
+            worker1.Filling("Ivanov Ivan", 25, "manager", 8);   //Полный метод
+            double pay = worker1.Payroll(); //Можно получить если поставить public в методе класса
+            Console.WriteLine("{0}", pay.ToString());
+            worker1.PrintInfo();
+
+            Console.WriteLine();
+            Worker worker2 = new Worker();
+            worker2.Filling("waiter", 2);   //Сокращенный перегруженный метод
+            worker2.PrintInfo();
+
+            Console.WriteLine();
+            Worker worker3 = new Worker();
+            worker3.Filling("dishwasher", 0);   //Сокращенный перегруженный метод
+            worker3.Name = "Petrov Petr";       // + Свойства
+            worker3.Age = 12;                   //
+            Console.WriteLine("Имя: {0}", worker3.Name);
+            worker3.PrintInfo();
+
+            Console.WriteLine();
+            Worker worker4 = new Worker();
+            worker4.Filling("Sidorov Sidr", 32, "cook", 10);    //Полный метод
+            string str = "Специальность: " + worker4["speciality"] + "\t Опыт работы: " + worker4["experience"];    // Индексатор
+            Console.WriteLine("Имя: {0} \t {1}", worker4.Name, str);
+
+            Console.WriteLine();
+            Console.WriteLine("Всего работников: {0}", Worker.Counter.ToString());  // Статический "счетчик"
+
+            //Конец:
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
