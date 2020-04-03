4a3f028ed28281f91361e946cf668e9ffd6ae1e1
953506 <62437619+953506@users.noreply.github.com>
2020-04-03 17:55:34 +0300

    добавил лабы

---
+++ maksim.ganusevich/laba2/task_delimost_na_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int counter = 0;
+            BigInteger startValue=0, endValue=0, composition=1;
+            do
+            {
+                Console.WriteLine("Введите начальное значение и конечное");
+                startValue = Convert.ToUInt64(Console.ReadLine());
+                endValue = Convert.ToUInt64(Console.ReadLine());
+            }
+            while (startValue>endValue);
+           
+            if (startValue % 2 == 1)
+                startValue++;
+            if (startValue != 0)
+            {
+                for (BigInteger value = startValue; value <= endValue; value += 2)
+                {
+                    BigInteger value0 = value;
+                    while (value0 % 2 == 0)
+                    {
+                        value0 /= 2;
+                        counter++;
+                    }
+                }
+                Console.WriteLine($"Будет делиться на 2 в {counter} степени");
+
+                // чтобы здесь был хоть какой-то смысл в использовании BigInteger, а не long я сделал второй вариант, но он очень медленный
+
+                for (BigInteger value = startValue; value <= endValue; value += 2)
+                    composition *= value;
+                
+                counter = 0;
+                while (composition % 2 == 0)
+                {
+                    composition /= 2;
+                    counter++;
+                }
+                Console.WriteLine($"Будет делиться на 2 в {counter} степени");
+            }
+            else
+                Console.WriteLine("Будет делиться на 2 в бесконечной степени степени");
+
+            // первым способом считает быстро до 10 в 7 степени, вторым - 10 в 5 считает уже секунд 30
+        }
+    }
+}
---
+++ maksim.ganusevich/laba3/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace laba3
+{
+    class Human
+    {
+        private enum Gender 
+        { 
+            male,
+            female,
+            indefined
+        }
+        
+        public DateTime date_of_death { get; set; }
+        public DateTime date_of_birth { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+        public string name { get; set; }
+        public String surname { get; set; }
+        public string eye_color { get; set; }
+        public string hair_color { get; set; }
+        public string nationality { get; set; }
+        public int id { set; }
+        public Gender _gender { get; set; }
+        public bool _isLife { get; set; }
+        private DateTime _age;
+        private static int newID = 0;
+
+
+        public Human()
+        {
+            _isLife = false;
+            _age = 0;
+            date_of_birth();
+            height = 0;
+            weight = 0;
+            name = "-";
+            surname = "-";
+            eye_color = "-";
+            hair_color = "-";
+            nationality = "-";
+            id = newID;
+            newID++;
+            date_of_death();
+            _gender = Gender.indefined;
+        }
+
+        public Human(int day, int month, int year, int height, int weight, string name, string surname, string eye_color, string hair_color, string nationality, int day_of_death, int month_of_death, int year_of_death, Gender gender) 
+        {
+            this.date_of_birth.Day = day;
+            this.date_of_birth.Month = month;
+            this.date_of_birth.Year = year;
+            this.height = height;
+            this.weight = weight;
+            this.name = name;
+            this.surname = surname;
+            this.eye_color = eye_color;
+            this.hair_color = hair_color;
+            this.nationality = nationality;
+            this.date_of_death.Day = day_of_death;
+            this.date_of_dea.Month = month_of_death;
+            this.date_of_death.Year = year_of_death;
+            _gender = gender;
+            _age = date_of_death - date_of_birth;
+            _isLife = false;
+            id = newID;
+            newID++;
+        }
+
+        public Human(int day, int month, int year, int height, int weight, string name, string surname, string eye_color, string hair_color, string nationality, Gender gender)
+        {
+            this.date_of_birth.Day = day;
+            this.date_of_birth.Month = month;
+            this.date_of_birth.Year = year;
+            this.height = height;
+            this.weight = weight;
+            this.name = name;
+            this.surname = surname;
+            this.eye_color = eye_color;
+            this.hair_color = hair_color;
+            this.nationality = nationality;
+            _age = DateTime.Now - date_of_birth;
+            _isLife = true;
+            id = newID;
+            newID++;
+        }
+
+        public int getAge()
+        {
+            return _age.Year;
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine(@$"
+    Возраст: {Convert.ToString(_age.Year)}
+    Дата рождения: {Convert.ToString(date_of_birth)}
+    Рост: {Convert.ToString(height)}
+    Вес: {Convert.ToString(weight)}
+    Имя: {name}
+    Фамилия: {surname}
+    Цвет глаз: {eye_color}
+    Цвет волос:{hair_color}
+    Национальность:{nationality}
+    id: {id}");
+        }
+
+        public void setInfo(int day, int month, int year, int height, int weight, string name, string surname, string eye_color, string hair_color, string nationality, int day_of_death, int month_of_death, int year_of_death)
+        {
+            this.date_of_birth.Day = day;
+            this.date_of_birth.Month = month;
+            this.date_of_birth.Year = year;
+            this.height = height;
+            this.weight = weight;
+            this.name = name;
+            this.surname = surname;
+            this.eye_color = eye_color;
+            this.hair_color = hair_color;
+            this.nationality = nationality;
+            this.date_of_death.Day = day_of_death;
+            this.date_of_dea.Month = month_of_death;
+            this.date_of_death.Year = year_of_death;
+            _age = date_of_death - date_of_birth;
+            _isLife = false;
+        }
+
+        public void setInfo()
+        {
+
+        }
+
+        public void Death(int day_of_death, int month_of_death, int year_of_death) 
+        {
+            _isLife = false;
+            this.date_of_death.Day = day_of_death;
+            this.date_of_dea.Month = month_of_death;
+            this.date_of_death.Year = year_of_death;
+            _age = date_of_death - date_of_birth;
+        }
+
+
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Human human = new Human();
+            human.Age = 10;
+            human.setInfo();
+
+        }
+    }
+}
