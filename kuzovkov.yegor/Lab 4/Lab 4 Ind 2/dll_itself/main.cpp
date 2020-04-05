53253bb7802785dd454aa68fe7d15618de4a7575
BoyNextDoorxBY <egkuzoff@gmail.com>
2020-04-05 20:25:39 +0300

    Fixed all the methods.
    All the methods now have calling conventions.

--- kuzovkov.yegor/Lab 4/Lab 4 Ind 2/dll_itself/main.cpp
+++ kuzovkov.yegor/Lab 4/Lab 4 Ind 2/dll_itself/main.cpp
@@ -2,25 +2,25 @@
 #include "C:\Users\\George_PC\Desktop\Labs\sem 2\ISP\Lab 4\Lab 4 Ind 2\Support\myHeader.h"
 #include "C:\Users\George_PC\Desktop\Labs\sem 2\ISP\Lab 4\Lab 4 Ind 2\Support\mySource.cpp"
 
-extern "C" __declspec(dllexport) double Add(double var_x, double var_y)
+extern "C" __declspec(dllexport) __stdcall double Add(double var_x, double var_y)
 {
 	MathClass MC(var_x, var_y);
 	return MC.Add();
 }
 
-extern "C" __declspec(dllexport) double Subtract(double var_x, double var_y)
+extern "C" __declspec(dllexport) __stdcall double Subtract(double var_x, double var_y)
 {
 	MathClass MC(var_x, var_y);
 	return MC.Subtract();
 }
 
-extern "C" __declspec(dllexport) double Compose(double var_x, double var_y)
+extern "C" __declspec(dllexport) __fastcall double Compose(double var_x, double var_y)
 {
 	MathClass MC(var_x, var_y);
 	return MC.Compose();
 }
 
-extern "C" __declspec(dllexport) double Divide(double var_x, double var_y)
+extern "C" __declspec(dllexport) __fastcall double Divide(double var_x, double var_y)
 {
 	MathClass MC(var_x, var_y);
 	return MC.Divide();
