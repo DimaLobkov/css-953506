736b96204c935856180664fcc57fc695f78c47fb
Sassiq <62505206+Sassiq@users.noreply.github.com>
2020-04-12 02:41:02 +0300

    Add files via upload

---
+++ Chekhovich Stanislav/Lab3/Firearm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class Firearm
+    {
+        private string _model;
+        private string _caliber;
+        private uint _maxAmmo;
+        private uint _ammoInClip;
+        private bool _reload;
+        private bool _safety = true;
+        private static string _generalInfo = "Firearm weapon use ammo to shoot";
+        private bool[] _mode = new bool[3] { true, false, false };
+        public bool Reload
+        {
+            get
+            {
+                return _reload;
+            }
+            set
+            {
+                _ammoInClip = _maxAmmo;
+                _reload = value;
+            }
+        }
+        public bool Safety
+        {
+            get
+            {
+                return _safety;
+            }
+            set
+            {
+                _safety = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public string Caliber
+        {
+            get
+            {
+                return _caliber;
+            }
+            set
+            {
+                _caliber = value;
+            }
+        }
+        public uint MaxAmmo
+        {
+            get
+            {
+                return _maxAmmo;
+            }
+            set
+            {
+                _maxAmmo = value;
+            }
+        }
+        public uint AmmoInClip
+        {
+            get
+            {
+                return _ammoInClip;
+            }
+            set
+            {
+                _ammoInClip = value;
+                _reload = _ammoInClip == 0;
+            }
+        }
+        public bool this[int index]
+        {
+            get
+            {
+                return _mode[index];
+            }
+            set
+            {
+                _mode[0] = _mode[1] = _mode[2] = false;
+                _mode[index] = value;
+            }
+        }
+        public Firearm(string model, string caliber, uint maxAmmo, uint ammoInClip)
+        {
+            _model = model;
+            _caliber = caliber;
+            _maxAmmo = maxAmmo;
+            _ammoInClip = maxAmmo < ammoInClip ? maxAmmo : ammoInClip;
+        }
+        public Firearm()
+        {
+            _model = "M16";
+            _caliber = "5,56";
+            _maxAmmo = 30;
+            _ammoInClip = 30;
+        }
+        public void ReloadFirearm(uint ammo)
+        {
+            _ammoInClip = ammo > _maxAmmo ? _maxAmmo : ammo;
+            _reload = false;
+        }
+        public void Shoot()
+        {
+            if (_ammoInClip > 0)
+            {
+                if (_mode[0])
+                {
+                    _ammoInClip -= 1;
+                    Console.WriteLine($"PAU! Ammo in clip: {_ammoInClip}/{_maxAmmo}");
+                }
+                if(_mode[1])
+                {
+                    if(_ammoInClip > 2)
+                    {
+                        _ammoInClip -= 3;
+                        Console.WriteLine($"PAU! PAU! PAU! Ammo in clip: {_ammoInClip}/{_maxAmmo}");
+                    }
+                    else if(_ammoInClip == 2)
+                    {
+                        _ammoInClip -= 2;
+                        Console.WriteLine($"PAU! PAU! Ammo in clip: {_ammoInClip}/{_maxAmmo}");
+                    }
+                    else if(_ammoInClip == 1)
+                    {
+                        _ammoInClip -= 1;
+                        Console.WriteLine($"PAU! Ammo in clip: {_ammoInClip}/{_maxAmmo}");
+                    }
+                }
+                if(_mode[2])
+                {
+                    _ammoInClip -= 1;
+                    Console.Write($"PAU! {_ammoInClip}/{_maxAmmo} ");
+                }
+            }
+            else
+            {
+                _reload = true;
+            }    
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"{_generalInfo}" +
+                $"\nModel of your weapon: {_model}" +
+                $"\nCaliber of your weapon: {_caliber}" +
+                $"\nMax ammo in a clip: {_maxAmmo}" +
+                $"\nCurrent ammo in a clip: {_ammoInClip}");
+        }
+    }
+}
---
+++ Chekhovich Stanislav/Lab3/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a, ammo, K = 1;
+            bool exit = false;
+            Firearm Gun = new Firearm();
+            while (true)
+            {
+                Console.WriteLine("1.Create a new firearm" +
+                    "\n2.Shoot a firearm" +
+                    "\n3.Realod a firearm" +
+                    "\n4.Info about gun" +
+                    "\n5.Switch the safety" +
+                    "\n6.Switch mode" +
+                    "\n7.Change info" +
+                    "\n8.Exit the program");
+                if (!UInt32.TryParse(Console.ReadLine(), out K))
+                {
+                    Console.WriteLine("Error");
+                    break;
+                }
+                Console.Clear();
+                switch (K)
+                {
+                    case 1:
+                        {
+                            uint maxAmmo, ammoInClip;
+                            Console.Write("Write model of your weapon: ");
+                            string model = Console.ReadLine();
+                            Console.Write("Write caliber of your weapon: ");
+                            string caliber = Console.ReadLine();
+                            Console.Write("Write max ammo in a clip: ");
+                            if(!UInt32.TryParse(Console.ReadLine(), out maxAmmo))
+                            {
+                                Console.WriteLine("Error");
+                                break;
+                            }
+                            Console.Write("Write current ammo in a clip: ");
+                            if (!UInt32.TryParse(Console.ReadLine(), out ammoInClip))
+                            {
+                                Console.WriteLine("Error");
+                                break;
+                            }
+                            Gun = new Firearm(model, caliber, maxAmmo, ammoInClip);
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (Gun.Safety)
+                            {
+                                Console.WriteLine("Turn the safety off before shooting");
+                            }
+                            else
+                            {
+                                while (!Gun.Reload)
+                                {
+                                    Gun.Shoot();
+                                }
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("How many ammo do you want to reload?" +
+                                "\n1.Full clip" +
+                                "\n2.My number");
+                            if (!UInt32.TryParse(Console.ReadLine(), out a))
+                            {
+                                Console.WriteLine("Error");
+                                break;
+                            }
+                            if(a == 1)
+                            {
+                                Gun.Reload = false;
+                            }
+                            else if (a == 2)
+                            {
+                                Console.WriteLine("What number?");
+                                if (!UInt32.TryParse(Console.ReadLine(), out ammo))
+                                {
+                                    Console.WriteLine("Error");
+                                    break;
+                                }
+                                Gun.ReloadFirearm(ammo);
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            Gun.GetInfo();
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("1.Turn the safety off" +
+                                "\n2.Turn the safety on");
+                            if (!UInt32.TryParse(Console.ReadLine(), out a))
+                            {
+                                Console.WriteLine("Error");
+                                break;
+                            }
+                            switch(a)
+                            {
+                                case 1:
+                                    {
+                                        Gun.Safety = false;
+                                        Console.WriteLine("You've got the safety off");
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Gun.Safety = true;
+                                        Console.WriteLine("You've got the safety on");
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("1.Single" +
+                                "\n2.Burst" +
+                                "\n3.Auto");
+                            if (!UInt32.TryParse(Console.ReadLine(), out a))
+                            {
+                                Console.WriteLine("Error");
+                                break;
+                            }
+                            switch (a)
+                            {
+                                case 1:
+                                    {
+                                        Gun[0] = true;
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Gun[1] = true;
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        Gun[2] = true;
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.Write("Write new model of your weapon: ");
+                            Gun.Model = Console.ReadLine();
+                            Console.Write("Write new caliber of your weapon: ");
+                            Gun.Caliber = Console.ReadLine();
+                            Console.Write("Write new max ammo in a clip: ");
+                            if (!UInt32.TryParse(Console.ReadLine(), out ammo))
+                            {
+                                Console.WriteLine("Error");
+                                break;
+                            }
+                            Gun.MaxAmmo = ammo;
+                            Console.Write("Write new current ammo in a clip: ");
+                            if (!UInt32.TryParse(Console.ReadLine(), out ammo))
+                            {
+                                Console.WriteLine("Error");
+                                break;
+                            }
+                            Gun.AmmoInClip = ammo > Gun.MaxAmmo ? Gun.MaxAmmo : ammo;
+                            break;
+                        }
+                    case 8:
+                        {
+                            exit = true;
+                            break;
+                        }
+                }
+                if (exit == true)
+                {
+                    break;
+                }
+                Console.WriteLine("\nPress any button...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
