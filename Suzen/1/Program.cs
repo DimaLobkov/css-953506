84ec4380aad4c41008dbb86f7d44ff0e45440a5e
953506 <62437619+953506@users.noreply.github.com>
2020-04-18 09:08:38 +0300

    Add files via upload

---
+++ Suzen/1/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> count1 = new List<int>() { 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 11, 11, 11, 11 };
+            bool ifer = false;
+            int randomer = 0, Sum = 0, check = 0, randomer2 = 0, Sum2 = 0, rand2 = 0, a = 35;
+            Random rand = new Random();
+
+            if (!ifer)
+            {
+                while (true)
+                {
+                    rand2 = rand.Next() % 2;
+                    if (rand2 == 1)
+                    {
+                        Sum2++;
+                    }
+                    else
+                    {
+                        Sum2--;
+                    }
+                    randomer = rand.Next() % a;
+                    Sum += count1[randomer];
+                    randomer2 = rand.Next() % a;
+                    Sum2 += count1[randomer];
+
+                    Console.WriteLine("Your cart is");
+                    switch (count1[randomer])
+                    {
+                        case 6:
+                            Console.WriteLine("|6|");
+                            break;
+                        case 7:
+                            Console.WriteLine("|7|");
+                            break;
+                        case 8:
+                            Console.WriteLine("|8|");
+                            break;
+                        case 9:
+                            Console.WriteLine("|9|");
+                            break;
+                        case 10:
+                            Console.WriteLine("|10|");
+                            break;
+                        case 3:
+                            Console.WriteLine("|J|");
+                            break;
+                        case 4:
+                            Console.WriteLine("|Q|");
+                            break;
+                        case 5:
+                            Console.WriteLine("|K|");
+                            break;
+                        case 11:
+                            Console.WriteLine("|A|");
+                            break;
+                    }
+                    count1.RemoveAt(randomer);
+                    a--;
+
+                    Console.WriteLine("You have {0} points", Sum);
+                    if (Sum2 > 21 && Sum < 21)
+                    {
+                        Console.WriteLine("YOU WIN");
+                        ifer = true;
+                        break;
+
+                    }
+                    if (Sum > 21)
+                    {
+                        Console.WriteLine("YOU LOSE");
+                        ifer = true;
+                        break;
+                    }
+
+                    if (Sum == 21)
+                    {
+                        Console.WriteLine("YOU WIN");
+                        ifer = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Do you want to take one more card?");
+                        Console.WriteLine("|YES(1)|NO(0)|");
+                        check = Convert.ToInt32(Console.ReadLine());
+                        if (check == 0)
+                        {
+                            Console.WriteLine("Computer have {0} points", Sum2);
+
+                            if (Sum > Sum2)
+                            {
+                                Console.WriteLine("YOU WIN");
+                                ifer = true;
+                                break;
+                            }
+                            else
+                            {
+                                if (Sum == Sum2)
+                                {
+                                    Console.WriteLine("IT IS A DRAW");
+                                    ifer = true;
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("YOU LOSE");
+                                    ifer = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Suzen/2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Numerics;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (exit == false)
+            {
+                Console.WriteLine("Choose the task from 1 to 3(for quit - 0):");
+                int choose = Convert.ToInt32(Console.ReadLine());
+                switch (choose)
+                {
+                    case 0:
+                        exit = true;
+                        break;
+
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine(DateTime.Now.ToString("dd MM yyyy HH:mm:ss"));
+                        Console.WriteLine(DateTime.Now.ToString("dd MMMM yyyy hh:mm:ss"));
+                        string nowDate = DateTime.Now.ToString();
+                        int sum = 0;
+                        for (char num = '0'; num <= '9'; num++)
+                        {
+                            for (int r = 0; r < nowDate.Length; r++)
+                            {
+                                if (nowDate[r] == num)
+                                {
+                                    sum++;
+                                }
+                            }
+
+                            Console.WriteLine("{0}:{1}", num, sum);
+                            sum = 0;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Random rnd = new Random();
+                        string[] storage = { "a","b","c","d","e","f","g",
+                                     "h","i","j","k","l","m","n",
+                                     "o","p","q","v","r","s","t",
+                                     "u","v","w","x","y","z"};
+                        Console.Write("Random line: ");
+                        for (int r = 0; r < 4; r++)
+                        {
+                            int letter = rnd.Next(storage.Length);
+                            Console.Write(storage[letter]);
+                        }
+                        Console.Write("\n");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Write interval [a,b]\na=");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("b=");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        BigInteger i;
+                        for (i = 1; a <= b; a++)
+                        {
+
+                            i = i * a;
+
+                        }
+                        Console.Write("Your number :{0}\n", i);
+                        int score = 0;
+                        BigInteger j;
+                        for (; ; )
+                        {
+
+                            j = i % 2;
+                            i /= 2;
+                            if (j == 0)
+                            {
+                                score++;
+                            }
+                            else break;
+
+                        }
+                        Console.Write("The highest degree of 2:{0}\n\n", score);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
