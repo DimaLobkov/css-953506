b98ed50a740949dea01d4c80e7fe2fc15fb70f0c
MBindich <mbindich@gmail.com>
2020-04-19 13:16:05 +0300

    Add LAB 4 task 1

---
+++ bindich.mikhail/LAB 4/1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _4._1
+{
+    class Program
+    {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string Cmd, StringBuilder StrReturn, int ReturnLength, IntPtr HwndCallback);
+
+        static void playWav(string fileName)
+        {
+            mciSendString("open \"" + fileName + "\" type waveaudio alias thisIsMyTag", null, 0, IntPtr.Zero);
+            mciSendString("play thisIsMyTag from 0", null, 0, IntPtr.Zero);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path to .wav file you want to open:");
+            string fileName = Console.ReadLine();
+
+            playWav(fileName);
+
+            Console.WriteLine("Press any button to close the program");
+            Console.ReadKey();
+        }
+    }
+}
