19faa48d5da80cb0e235180ca5821a124fadaeb8
DimaLobkov <63234616+DimaLobkov@users.noreply.github.com>
2020-05-02 00:39:03 +0300

    Add files via upload

---
+++ Lobkov.Dmitry/LW-4/4.1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.InteropServices;   
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options); 
+
+        static void Main()
+        {
+            int round, urScore = 0, compScore = 0;
+
+            Random rnd = new Random();
+
+            Console.Write("\tRock-paper-scissors\nYour task is to beat the computer!\n\nEnter the number of rounds: ");
+            while (!int.TryParse(Console.ReadLine(), out round))
+            {
+                Console.WriteLine("\n\tIncorrect input. Try again");
+            }
+
+            Console.WriteLine("\n\tGame on!!!");
+            for (int counter = 0; counter < round; counter++)
+            {
+                int urSelect, compSelect;
+
+                Console.WriteLine("\nSelect\n1) Rock\n2) Paper\n3) Scissors\n4) To surrender");
+                while (!int.TryParse(Console.ReadLine(), out urSelect))
+                {
+                    Console.WriteLine("\n\tIncorrect input. Try again");
+                }
+                
+                compSelect = rnd.Next(1, 4);
+                if (urSelect == 4)
+                {
+                    Console.WriteLine("\nAutomatic defeat");
+                    return;
+                }
+
+                else if ((urSelect - 1 == compSelect) || (urSelect == 1 && compSelect == 3))
+                {
+                    urScore++;
+                    MessageBox(IntPtr.Zero, "YOU WON!", $"the results of the round {counter + 1}", 0);
+                }
+
+                else if(urSelect == compSelect)
+                {
+                    urScore++;
+                    compScore++;
+                    MessageBox(IntPtr.Zero, "Tie", $"the results of the round {counter + 1}", 0);
+                }
+
+                else
+                {
+                    compScore++;
+                    MessageBox(IntPtr.Zero, "You lost...", $"the results of the round {counter + 1}", 0);
+                }
+
+                Console.Clear();
+                Console.WriteLine($"Your score: {urScore}\nComputer score: {compScore}");
+            }
+
+            if(urScore > compScore)
+            {
+                MessageBox(IntPtr.Zero, "YOU ARE WINNER!", $"score {urScore}:{compScore}", 0);
+            }
+
+            else if (urScore < compScore)
+            {
+                MessageBox(IntPtr.Zero, "COMPUTER ARE WINNER!", $"score {urScore}:{compScore}", 0);
+            }
+
+            else
+            {
+                MessageBox(IntPtr.Zero, "TIE", $"score {urScore}:{compScore}", 0);
+            }
+        }
+    }
+}
---
+++ Lobkov.Dmitry/LW-4/dll/dll.cpp
@@ -0,0 +1,8 @@
+﻿#include "pch.h"
+#pragma hdrstop
+#pragma argsused
+
+extern "C" float __declspec(dllexport) __stdcall VolumePrlpd(float a, float b, float c) 
+{
+	return a * b * c;
+}
