8525949a20b595cd4e3a301040d6543b6558ccba
DimaLobkov <63234616+DimaLobkov@users.noreply.github.com>
2020-05-27 15:39:13 +0300

    Add files via upload

---
+++ Lobkov.Dmitry/LW-7/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7
+{
+    class Fraction : IComparable, IEquatable<Fraction>
+    {
+        //поля
+        private int _numerator;
+        private int _denominator;
+
+        //конструктор
+        public Fraction(int numerator, int denominator)
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+
+        //методы
+        private int EuclideanAlgorithm(int num1, int num2)
+        {
+            if (num1 % num2 == 0)
+            {
+                return num2;
+            }
+
+            if (num2 % num1 == 0)
+            {
+                return num1;
+            }
+
+            if (num1 > num2)
+            {
+                return EuclideanAlgorithm(num1 % num2, num2);
+            }
+
+            return EuclideanAlgorithm(num1, num2 % num1);
+        }
+
+        private void Simplify()
+        {
+            if (_numerator != 0)
+            {
+                int devider = EuclideanAlgorithm(Math.Abs(_numerator), Math.Abs(_denominator));
+                _numerator /= devider;
+                _denominator /= devider;
+            }
+        }
+
+        //операторы
+        public static Fraction operator +(Fraction num1, Fraction num2)
+        {
+            Fraction total = new Fraction(num1._numerator * num2._denominator + num2._numerator * num1._denominator, num1._denominator * num2._denominator);
+            return total;
+        }
+
+        public static Fraction operator -(Fraction num1, Fraction num2)
+        {
+            Fraction total = new Fraction(num1._numerator * num2._denominator - num2._numerator * num1._denominator, num1._denominator * num2._denominator);
+            return total;
+        }
+
+        public static Fraction operator *(Fraction num1, Fraction num2)
+        {
+            Fraction total = new Fraction(num1._numerator * num2._numerator, num1._denominator * num2._denominator);
+            return total;
+        }
+
+        public static Fraction operator /(Fraction num1, Fraction num2)
+        {
+            Fraction total = new Fraction(num1._numerator * num2._denominator, num1._denominator * num2._numerator);
+            return total;
+        }
+
+        public static explicit operator double(Fraction fr)
+        {
+            return (double)fr._numerator / fr._denominator;
+        }
+
+        public static implicit operator int(Fraction fr)
+        {
+            return fr._numerator / fr._denominator;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Fraction anotherFraction = obj as Fraction;
+
+            if ((double)_numerator / _denominator < anotherFraction)
+                return -1;
+            if ((double)_numerator / _denominator > anotherFraction)
+                return 1;
+            return 0;
+        }
+
+        public bool Equals(Fraction anotherFraction)
+        {
+            if (anotherFraction is null) 
+            { 
+                return false;
+            }
+
+            return (double)_numerator / _denominator == anotherFraction;
+        }
+
+        public static bool operator <(Fraction num1, Fraction num2)
+        {
+            double a = (double)num1, b = (double)num2;
+            return a < b;
+        }
+
+        public static bool operator >(Fraction num1, Fraction num2)
+        {
+            double a = (double)num1, b = (double)num2;
+            return a > b;
+        }
+
+        public static bool operator >=(Fraction num1, Fraction num2)
+        {
+            double a = (double)num1, b = (double)num2;
+            return a >= b;
+        }
+
+        public static bool operator <=(Fraction num1, Fraction num2)
+        {
+            double a = (double)num1, b = (double)num2;
+            return a <= b;
+        }
+
+        public static bool operator ==(Fraction num1, Fraction num2)
+        {
+            return num1.Equals((object)num2);
+        }
+
+        public static bool operator !=(Fraction num1, Fraction num2)
+        {
+            return !num1.Equals((object)num2);
+        }
+
+        public override string ToString()
+        {
+            Simplify();
+            return ($"{_numerator}/{_denominator}");
+        }
+
+        //деструктор
+        ~Fraction() { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction firstNum, secondNum;
+            int numerator, denominator;
+            bool getOut = false;
+
+            do
+            {
+                Console.WriteLine("Enter numerator and then denominator of the 1st number: ");
+                int.TryParse(Console.ReadLine(), out numerator);
+                int.TryParse(Console.ReadLine(), out denominator);
+            } while ((numerator == 0) || (denominator == 0));
+
+            firstNum = new Fraction(numerator, denominator);
+            do
+            {
+                Console.WriteLine("Enter numerator and then denominator of the 2nd number: ");
+                int.TryParse(Console.ReadLine(), out numerator);
+                int.TryParse(Console.ReadLine(), out denominator);
+            } while ((numerator == 0) || (denominator == 0));
+
+            secondNum = new Fraction(numerator, denominator);
+            do
+            {
+                int select;
+
+                Console.Clear();
+                do
+                {
+                    Console.WriteLine("1) Addition\n2) Substraction\n3) Composition\n4) Devision\n");
+                    Console.WriteLine("5) <\n6) >\n7) <=\n8) >=\n9) ==\n10) !=\n11) Exit\nYour select: ");
+                    int.TryParse(Console.ReadLine(), out select);
+                    Console.Clear();
+                } while (select == -1);
+                switch (select)
+                {
+                    case 1:
+                        Console.WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum} = {(double)(firstNum + secondNum)}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{firstNum} - {secondNum} = {firstNum - secondNum} = {(double)(firstNum - secondNum)}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum} = {(double)(firstNum * secondNum)}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{firstNum} / {secondNum} = {firstNum / secondNum} = {(double)(firstNum / secondNum)}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"{firstNum} < {secondNum} is {firstNum < secondNum}");
+                        break;
+                    case 6:
+                        Console.WriteLine($"{firstNum} > {secondNum} is {firstNum > secondNum}");
+                        break;
+                    case 7:
+                        Console.WriteLine($"{firstNum} <= {secondNum} is {firstNum <= secondNum}");
+                        break;
+                    case 8:
+                        Console.WriteLine($"{firstNum} >= {secondNum} is {firstNum >= secondNum}");
+                        break;
+                    case 9:
+                        Console.WriteLine($"{firstNum} == {secondNum} is {firstNum == secondNum}");
+                        break;
+                    case 10:
+                        Console.WriteLine($"{firstNum} != {secondNum} is {firstNum != secondNum}");
+                        break;
+                    case 11:
+                        getOut = true;
+                        break;
+                    default:
+                        Console.WriteLine("Something is wrong");
+                        break;
+                }
+
+                Console.WriteLine("Press any key..");
+                Console.ReadKey();
+            } while (!getOut);
+
+            Console.Clear();
+            Console.WriteLine("Example");
+            Fraction a = new Fraction(7, 3);
+            Fraction b = new Fraction(-6, 5);
+            Fraction c = new Fraction(3, 7);
+            Fraction d = new Fraction(-5, 6);
+            var list = new List<Fraction>() { a, b, c, d };
+
+            foreach (var num in list)
+            {
+                Console.WriteLine(num.ToString());
+            }
+
+            list.Sort();
+            Console.WriteLine("\nSorted nums:");
+            foreach (var num in list)
+            {
+                Console.WriteLine(num.ToString());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lobkov.Dmitry/LW-8/Program.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace laba
+{
+    interface ISpecifications
+    {
+        int Experience { get; set; }
+        string IqGet { get; }
+        int IqSet { set; }
+        string Recommend { get; set; }
+        string Scheduled { get; set; }
+    }
+
+    struct Fullname
+    {
+        public string firstname, lastname, middlename;
+    }
+
+    class Human
+    {
+        //поля
+        protected Fullname fullname;
+        protected bool _isConvicted;
+
+        //конструктор
+        public Human(string firstname, string lastname, string middlename)
+        {
+            fullname.firstname = firstname;
+            fullname.lastname = lastname;
+            fullname.middlename = middlename;
+        }
+
+        //свойства
+        public string ShowFullname
+        {
+            get
+            {
+                return $"{fullname.lastname} {fullname.firstname} {fullname.middlename}";
+            }
+        }
+
+        public string IsConvicted
+        {
+            get
+            {
+                if (_isConvicted)
+                {
+                    return "yes";
+                }
+
+                else
+                {
+                    return "no";
+                }
+            }
+
+            set
+            {
+                if (value == "yes")
+                {
+                    Console.WriteLine("Was it a suspended sentence? (\"yes\" or \"no\")");
+                    string isSuspended = Console.ReadLine();
+                    if (isSuspended == "yes")
+                    {
+                        Console.WriteLine("How many years have passed since the verdict?");
+                        int years;
+                        int.TryParse(Console.ReadLine(), out years);
+                        if (years > 9)
+                        {
+                            _isConvicted = false;
+                        }
+
+                        else _isConvicted = true;
+                    }
+
+                    else
+                    {
+                        _isConvicted = true;
+                    }
+                }
+
+                else
+                {
+                    _isConvicted = false;
+                }
+            }
+        }
+    }
+
+    class Employee : Human, ISpecifications
+    {
+        //поля
+        protected int _experience;
+        protected bool _isRecommend, _isUnscheduled;
+        protected IQ _iq;
+
+        //делегаты, события
+        public delegate void EmployeeHandler(string message);
+        public event EmployeeHandler Notify;
+        public delegate void Message();
+        public Message message;
+
+        //перечисление
+        public enum IQ
+        {
+            low = 1,
+            lowerMiddle,
+            middle,
+            upperMiddle,
+            high
+        }
+
+        //конструктор
+        public Employee(string firstname, string lastname, string middlename) : base(firstname, lastname, middlename) { }
+
+        //свойства
+        public int Experience
+        {
+            get
+            {
+                return _experience;
+            }
+
+            set
+            {
+                _experience = value;
+            }
+        }
+
+        public string IqGet
+        {
+            get
+            {
+                switch (_iq)
+                {
+                    case IQ.low:
+                        return "low IQ";
+                    case IQ.lowerMiddle:
+                        return "lower-middle IQ";
+                    case IQ.middle:
+                        return "middle IQ";
+                    case IQ.upperMiddle:
+                        return "upper-middle IQ";
+                    case IQ.high:
+                        return "high IQ";
+                    default:
+                        return "something gone wrong...";
+                }
+            }
+        }
+
+        public int IqSet
+        {
+            set
+            {
+                if (value < 70)
+                {
+                    _iq = IQ.low;
+                }
+
+                else if (value > 130)
+                {
+                    _iq = IQ.high;
+                }
+
+                else if (value > 110)
+                {
+                    _iq = IQ.upperMiddle;
+                }
+
+                else if (value > 90)
+                {
+                    _iq = IQ.middle;
+                }
+
+                else
+                {
+                    _iq = IQ.lowerMiddle;
+                }
+
+                Notify?.Invoke($"Employee has {_iq} IQ lvl");
+            }
+        }
+
+        public IQ Iq
+        {
+            get
+            {
+                return _iq;
+            }
+        }
+
+        public string Recommend
+        {
+            get
+            {
+                if (_isRecommend)
+                {
+                    return "yes";
+                }
+
+                else
+                {
+                    return "no";
+                }
+            }
+
+            set
+            {
+                if (value == "yes")
+                {
+                    _isRecommend = true;
+                    Notify?.Invoke("Employee has good reputation");
+                }
+
+                else
+                {
+                    _isRecommend = false;
+                    Notify?.Invoke("Employee has bad reputation");
+                }
+            }
+        }
+
+        public string Scheduled
+        {
+            get
+            {
+                if (_isUnscheduled)
+                {
+                    return "yes";
+                }
+
+                else
+                {
+                    return "no";
+                }
+            }
+
+            set
+            {
+                if (value == "yes")
+                {
+                    _isUnscheduled = true;
+                }
+
+                else
+                {
+                    _isUnscheduled = false;
+                }
+            }
+        }
+
+        public void Show(Message mes)
+        {
+            Console.WriteLine($"{fullname.lastname} {fullname.firstname} {fullname.middlename}\n" +
+            $"Is convicted? - {IsConvicted}\n" +
+            $"Experience: {Experience}\n" +
+            $"IQ: {IqGet}\n" +
+            $"Is recommended? - {Recommend}\n" +
+            $"Is unscheduled? - {Scheduled}");
+            mes?.Invoke();
+        }
+    }
+
+    class TheBestOne : IComparer<Employee>
+    {
+        public int Compare(Employee obj1, Employee obj2)
+        {
+            int counter1 = 0, counter2 = 0;
+
+            if (obj1.IsConvicted != obj2.IsConvicted)
+            {
+                if (obj1.IsConvicted == "no")
+                {
+                    counter1++;
+                }
+
+                else
+                {
+                    counter2++;
+                }
+            }
+
+            if (obj1.Experience > obj2.Experience)
+            {
+                counter1++;
+            }
+
+            else if (obj1.Experience < obj2.Experience)
+            {
+                counter2++;
+            }
+
+            if (obj1.Iq > obj2.Iq)
+            {
+                counter1++;
+            }
+
+            else if (obj1.Iq < obj2.Iq)
+            {
+                counter2++;
+            }
+
+            if (obj1.Recommend != obj2.Recommend)
+            {
+                if (obj1.Recommend == "yes")
+                {
+                    counter1++;
+                }
+
+                else
+                {
+                    counter2++;
+                }
+            }
+
+            if (obj1.Scheduled != obj2.Scheduled)
+            {
+                if (obj1.Scheduled == "yes")
+                {
+                    counter1++;
+                }
+
+                else
+                {
+                    counter2++;
+                }
+            }
+
+            if (counter1 > counter2)
+            {
+                return 1;
+            }
+
+            else if (counter2 > counter1)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+
+
+    class Program
+    {
+        public static void FirstEmp()
+        {
+            Console.WriteLine("It was the first employee.");
+        }
+
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Dmitry", "Lobkov", "Sergeevich");
+            Employee employee2 = new Employee("Ivan", "Ivanov", "Ivanovich");
+            employee1.Notify += mes => Console.WriteLine(mes);
+            employee2.Notify += delegate (string mes)
+            {
+                Console.WriteLine(mes);
+            };
+
+            try {
+                Console.Write($"Enter data about the first employee: {employee1.ShowFullname}\nIs convicted?(\"yes\" or \"no\"): ");
+                employee1.IsConvicted = Console.ReadLine();
+                Console.Write("Enter work experience: ");
+                employee1.Experience = int.Parse(Console.ReadLine());
+                Console.Write("Enter IQ: ");
+                employee1.IqSet = int.Parse(Console.ReadLine());
+                Console.Write("Is recommended?(\"yes\" or \"no\"): ");
+                employee1.Recommend = Console.ReadLine();
+                Console.Write("Is unscheduled?(\"yes\" or \"no\"): ");
+                employee1.Scheduled = Console.ReadLine();
+                Console.Write($"Enter data about the second employee: {employee2.ShowFullname}\nIs convicted?(\"yes\" or \"no\"): ");
+                employee2.IsConvicted = Console.ReadLine();
+                Console.Write("Enter work experience: ");
+                employee2.Experience = int.Parse(Console.ReadLine());
+                Console.Write("Enter IQ: ");
+                employee2.IqSet = int.Parse(Console.ReadLine());
+                Console.Write("Is recommended?(\"yes\" or \"no\"): ");
+                employee2.Recommend = Console.ReadLine();
+                Console.Write("Is unscheduled?(\"yes\" or \"no\"): ");
+                employee2.Scheduled = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("1)Information about the first employee\n2)Information about the second employee\n3)Compare");
+                int select = int.Parse(Console.ReadLine());
+                Console.Clear();
+                employee1.message += FirstEmp;
+                switch (select)
+                {
+                    case 1:
+                        employee1.Show(employee1.message);
+                        break;
+                    case 2:
+                        employee2.Show(employee1.message);
+                        break;
+                    case 3:
+                        TheBestOne theBestOne = new TheBestOne();
+                        int compareResult = theBestOne.Compare(employee1, employee2);
+                        Console.Write($"The recommended employee is ");
+                        if (compareResult == 1)
+                        {
+                            employee1.Show(employee1.message);
+                        }
+
+                        else if (compareResult == -1)
+                        {
+                            employee2.Show(employee1.message);
+                        }
+
+                        else
+                        {
+                            Console.Write($"both employees");
+                        }
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+} 
